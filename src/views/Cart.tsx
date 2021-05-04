70f8c9c9654e246e0e38edb41caa08d9c7ff4425
boushib <boushib01@gmail.com>
2021-05-04 01:54:22 +0000

    create the cart page content

--- src/views/Cart.tsx
+++ src/views/Cart.tsx
@@ -3,7 +3,40 @@ import './Cart.scss'
 const Cart: React.FC = () => (
   <div className="cart page">
     <div className="container">
-      <h1>Cart</h1>
+      <div className="cart__products">
+        <h1>Shopping Cart</h1>
+        <hr />
+        <p>Your amazon cart is empty!</p>
+        <div className="cart__product">
+          <img
+            className="cart__product__image"
+            src="https://images-na.ssl-images-amazon.com/images/I/71DnJGlQO2L._AC_UL405_SR405,405_.jpg"
+            alt=""
+          />
+          <div className="cart__product__details">
+            <div className="cart__product__title">
+              Huion Inspiroy H1060P Graphics Drawing Tablet with Tilt Response
+              Battery-Free Stylus and 8192 Pen Pressure Sensitivity
+            </div>
+            <div className="stock__status">In stock</div>
+            <div className="cart__product__footer">
+              <div className="quantity">Quantity: 1</div>
+              <div className="cart__links">
+                <div className="link">Delete</div>
+                <div className="link">Save for later</div>
+                <div className="link">Compare with similar items</div>
+              </div>
+            </div>
+          </div>
+          <div className="price">$66.99</div>
+        </div>
+      </div>
+      <div className="cart__subtotal">
+        <div className="amount">
+          Subtotal (1 item): <strong>$66.99</strong>
+        </div>
+        <button className="btn">Proceed to checkout</button>
+      </div>
     </div>
   </div>
 )
